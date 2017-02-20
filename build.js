2e0d4d02a07ab90c9ef97dad26d42d1e3888264a
Jackbennett <jbennett@birkdalehigh.co.uk>
2017-02-20 13:36:43 +0000

    V1 of a metalsmith example site

---
+++ build.js
@@ -0,0 +1,23 @@
+﻿const metalsmith = require('metalsmith')
+const handlebars = require('handlebars')
+const layouts = require('metalsmith-layouts')
+const permalinks = require('metalsmith-permalinks')
+
+handlebars.registerHelper('pretty', function (context) {
+    return JSON.stringify(context);
+});
+
+metalsmith(__dirname)
+    .source('./source/')
+    .destination('./public/')
+    .clean(true)
+    .use(layouts({
+        engine: 'handlebars',
+        default: 'default.hbs',
+        directory: './template/',
+        partials: './template/partial/'
+    }))
+    .use(permalinks({relative: false}))
+    .build(err =>{
+        if(err) throw err
+    })
